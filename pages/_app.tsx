0629d2166dc9a931a2441677a0f5c5a9c41fb26d
Ophtanass <chadli.nassim@gmail.com>
2025-10-13 13:45:12 +0200

    Update _app.tsx
    rajout CANONICAL

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
+// pages/_app.tsx
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  const canonical =
+    "https://portailmyopie.fr" +
+    (router.asPath === "/" ? "" : router.asPath.split("?")[0]);
+
+  return (
+    <>
+      <Head>
+        <link rel="canonical" href={canonical} />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
